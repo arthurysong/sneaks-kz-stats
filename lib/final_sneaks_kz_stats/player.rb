54ae2646d27046eec32c407dcb28b3e22dd72aac
Arthur Song <arth3rs0ng@gmail.com>
2019-04-16 07:02:45 +0000

    created class method all and new_five_from_url

--- lib/final_sneaks_kz_stats/player.rb
+++ lib/final_sneaks_kz_stats/player.rb
@@ -4,9 +4,16 @@ require "open-uri"
 
 class FinalSneaksKzStats::Player
   attr_accessor :name
+  @@all = []
   
-  def self.new_from_url(url)
-    
+  def self.all
+    @@all
+  end
+  
+  def self.new_five_from_url(url)
+    doc = open(url)
+    html = Nokogiri::HTML(doc)
+    html.css()
     
   end
 end
