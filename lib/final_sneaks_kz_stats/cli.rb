39f0207dce12517a991b46be1576eeb50c546208
Arthur Song <arth3rs0ng@gmail.com>
2019-04-16 06:50:13 +0000

    list players method

--- lib/final_sneaks_kz_stats/cli.rb
+++ lib/final_sneaks_kz_stats/cli.rb
@@ -5,7 +5,16 @@ class FinalSneaksKzStats::CLI
   
   def call
     puts "Stats for Sneak's KZ Server..."
+    list_players
   end
 
-
+  def list_players
+    puts "1. Player"
+    puts "2. Player"
+    puts "3. Player"
+    puts "4. Player"
+    puts "5. Player"
+    
+  end
+  
 end
