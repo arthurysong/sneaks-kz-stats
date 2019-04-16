d37bd608f7178fc1cd06bfb67036d6905cc72af9
Arthur Song <arth3rs0ng@gmail.com>
2019-04-16 06:57:52 +0000

    created player class

--- lib/final_sneaks_kz_stats.rb
+++ lib/final_sneaks_kz_stats.rb
@@ -1,5 +1,6 @@
 require_relative "./final_sneaks_kz_stats/version"
 require_relative "./final_sneaks_kz_stats/cli"
+require_relative "./final_sneaks_kz_stats/player"
 
 module FinalSneaksKzStats
   class Error < StandardError; end
---
+++ lib/final_sneaks_kz_stats/player.rb
@@ -0,0 +1,12 @@
+require "nokogiri"
+require "open-uri"
+
+
+class FinalSneaksKzStats::Player
+  attr_accessor :name
+  
+  def self.new_from_url(url)
+    
+    
+  end
+end
